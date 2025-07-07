927deab556744ffac70016305f4296039833e037
Adin <Adinstary@gmail.com>
2025-07-06 19:11:51 -0700

    Initial commit

---
+++ Source/GAM312_Smith/GAM312_Smith.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Smith.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Smith, "GAM312_Smith" );
