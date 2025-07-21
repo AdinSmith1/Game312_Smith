a958c16bea6427609133b2db1178a5d617e085f7
Adin <Adinstary@gmail.com>
2025-07-20 22:50:39 -0700

    Addeed building and a buy menu

--- Source/GAM312_Smith/PlayerChar.h
+++ Source/GAM312_Smith/PlayerChar.h
@@ -7,6 +7,7 @@
 #include "Camera/CameraComponent.h"
 #include "Resource_M.h"
 #include "Kismet/GameplayStatics.h"
+#include "BuildingPart.h"
 #include "PlayerChar.generated.h"
 
 UCLASS()
@@ -75,6 +76,18 @@ public:
 	UPROPERTY(EditAnywhere, Category = "HitMarker")
 		UMaterialInterface* hitDecal;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Supplies")
+		TArray<int> BuildingArray;
+
+	UPROPERTY()
+		bool isBuilding;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TSubclassOf<ABuildingPart> BuildPartClass;
+
+	UPROPERTY()
+		ABuildingPart* spawnedPart;
+
 	UFUNCTION(BlueprintCallable)
 		void SetHealth(float amount);
 
@@ -89,4 +102,13 @@ public:
 
 	UFUNCTION()
 		void GiveResource(float amount, FString resourceType);
+
+	UFUNCTION(BlueprintCallable)
+		void UpdateResources(float woodAmount, float stoneAmount, FString buildingObject);
+
+	UFUNCTION(BlueprintCallable)
+		void SpawnBuilding(int buildingID, bool& isSucess);
+
+	UFUNCTION()
+		void RotateBuilding();
 };
