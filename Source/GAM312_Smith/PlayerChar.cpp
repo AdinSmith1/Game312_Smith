b141adc91305d1abe8395f232e6dea97b86076b6
Adin <Adinstary@gmail.com>
2025-07-27 20:29:11 -0700

    Added Ai and status bars

--- Source/GAM312_Smith/PlayerChar.cpp
+++ Source/GAM312_Smith/PlayerChar.cpp
@@ -36,6 +36,10 @@ void APlayerChar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// update player stats
+	playerUI->UpdateBars(Health, Hunger, Stamina);
+
+	// Places object like walls
 	if (isBuilding)
 	{
 		if (spawnedPart) 
@@ -51,6 +55,7 @@ void APlayerChar::Tick(float DeltaTime)
 // Called to bind functionality to input
 void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	// Set Up player Keybinds
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerChar::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerChar::MoveRight);
@@ -63,23 +68,27 @@ void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("RotPart", IE_Pressed, this, &APlayerChar::RotateBuilding);
 }
 
+// Function to move player forward/back
 void APlayerChar::MoveForward(float axisValue)
 {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
 	AddMovementInput(Direction, axisValue);
 }
 
+// Function to move player Right/Left
 void APlayerChar::MoveRight(float axisValue)
 {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
 	AddMovementInput(Direction, axisValue);
 }
 
+// Function to start jump
 void APlayerChar::StartJump()
 {
 	bPressedJump = true;
 }
 
+// Function to stop jump
 void APlayerChar::StopJump()
 {
 	bPressedJump = false;
@@ -87,33 +96,46 @@ void APlayerChar::StopJump()
 
 void APlayerChar::FindObject()
 {
+
+	// Create vector from player camera
 	FHitResult HitResult;
 	FVector StartLocation = PlayerCamComp->GetComponentLocation();
 	FVector Direction = PlayerCamComp->GetForwardVector() * 800.0f;
 	FVector EndLocation = StartLocation + Direction;
 
+	// Setup collision with vector
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(this);
 	QueryParams.bTraceComplex = true;
 	QueryParams.bReturnFaceIndex = true;
 
+	// if player is not building
 	if (!isBuilding)
 	{
+		// Check for object infront of player
 		if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
 		{
 			AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor());
 
+
+			// checks to see if you have enough stamina to collect
 			if (Stamina > 5)
 			{
+				// checks if you hit a collectable object
 				if (HitResource)
 				{
+					// Get the name and amount of resource from the hit object
 					FString hitName = HitResource->resourceName;
 					int resourceValue = HitResource->resourceAmount;
 
+					// reduce amount of resources left in object
 					HitResource->totalResource = HitResource->totalResource - resourceValue;
 
+					// If the resource still has more left after collection
 					if (HitResource->totalResource > resourceValue)
 					{
+
+						//give resources and reduce stamina
 						GiveResource(resourceValue, hitName);
 
 						check(GEngine != nullptr);
@@ -123,8 +145,10 @@ void APlayerChar::FindObject()
 
 						SetStamina(-5.0f);
 					}
+					// If the resource has no more left after collection
 					else
 					{
+						// destroy resource and tell player
 						HitResource->Destroy();
 						check(GEngine != nullptr);
 						GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted"));
@@ -142,6 +166,8 @@ void APlayerChar::FindObject()
 	}
 }
 
+
+// function to add health to player might have a bug
 void APlayerChar::SetHealth(float amount)
 {
 	if (Health + amount < 100)
@@ -150,6 +176,7 @@ void APlayerChar::SetHealth(float amount)
 	}
 }
 
+// function to add hunger to player
 void APlayerChar::SetHunger(float amount)
 {
 	if (Hunger + amount < 100)
@@ -158,6 +185,7 @@ void APlayerChar::SetHunger(float amount)
 	}
 }
 
+// function to add stamina to player
 void APlayerChar::SetStamina(float amount)
 {
 	if (Stamina + amount < 100)
@@ -166,6 +194,7 @@ void APlayerChar::SetStamina(float amount)
 	}
 }
 
+// function to decrease hunger and health if hunger is 0 or lower
 void APlayerChar::DecreaseStats()
 {
 
@@ -182,6 +211,7 @@ void APlayerChar::DecreaseStats()
 	}
 }
 
+// Function to give player resources
 void APlayerChar::GiveResource(float amount, FString resourceType)
 {
 	if (resourceType == "Wood")
@@ -200,25 +230,32 @@ void APlayerChar::GiveResource(float amount, FString resourceType)
 	}
 }
 
+// Function to give player building objects if they have enough to make it
 void APlayerChar::UpdateResources(float woodAmount, float stoneAmount, FString buildingObject)
 {
+	// Check if you have enough wood
 	if (woodAmount <= ResourcesArray[0])
 	{
+		// Check if you have enough stone
 		if (stoneAmount <= ResourcesArray[1])
 		{
+			// Take stone and wood away
 			ResourcesArray[0] = ResourcesArray[0] - woodAmount;
 			ResourcesArray[1] = ResourcesArray[1] - stoneAmount;
 
+			// give Wall
 			if (buildingObject == "Wall")
 			{
 				BuildingArray[0] = BuildingArray[0] + 1;
 			}
 
+			// give Floor
 			if (buildingObject == "Floor")
 			{
 				BuildingArray[1] = BuildingArray[1] + 1;
 			}
 
+			// give Ceiling
 			if (buildingObject == "Ceiling")
 			{
 				BuildingArray[2] = BuildingArray[2] + 1;
@@ -226,13 +263,15 @@ void APlayerChar::UpdateResources(float woodAmount, float stoneAmount, FString b
 		}
 	}
 }
-
+// Function to spawn building
 void APlayerChar::SpawnBuilding(int buildingID, bool& isSucess)
 {
 	if (!isBuilding)
 	{
+		// Check to see if player has what they are trying to build
 		if (BuildingArray[buildingID] >= 1)
 		{
+			// Spawn building where player is currently looking and in current rotation
 			isBuilding = true;
 			FActorSpawnParameters SpawnParams;
 			FVector StartLocation = PlayerCamComp->GetComponentLocation();
@@ -251,6 +290,7 @@ void APlayerChar::SpawnBuilding(int buildingID, bool& isSucess)
 	}
 }
 
+// function to rotate building
 void APlayerChar::RotateBuilding()
 {
 	if (isBuilding)
